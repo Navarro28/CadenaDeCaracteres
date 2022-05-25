a5ebf581ed71902086eb26087c25284533657e4c
jose.raul.navarro.avila27@uabc.edu.mx <jose.raul.navarro.avila27@uabc.edu.mx>
2022-05-24 21:52:34 -0700

    Agregar archivos de proyecto.

---
+++ Cadenadecaracteres/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es tu nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Buenas tardes, hola" + " " + nombre + " " + "Le gustaria ver el menu? ");
+            Console.ReadKey();
+        }
+    }
+}
